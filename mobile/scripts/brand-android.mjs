88358c9ee82de82223873ccfaff77e41f40be304
mphatsonalikungwi <mphatsonalikungwi@gmail.com>
2026-09-16 14:49:27 +0200

    Add VMC Android launcher branding resources

---
+++ mobile/scripts/brand-android.mjs
@@ -0,0 +1,19 @@
+import { mkdir, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+const root = new URL('../android/', import.meta.url).pathname;
+const res = join(root, 'app', 'src', 'main', 'res');
+
+const files = {
+  'values/vmc_brand.xml': `<resources><color name="vmc_icon_background">#FF304F</color></resources>`,
+  'drawable/vmc_icon_foreground.xml': `<?xml version="1.0" encoding="utf-8"?><vector xmlns:android="http://schemas.android.com/apk/res/android" android:width="108dp" android:height="108dp" android:viewportWidth="108" android:viewportHeight="108"><path android:fillColor="#FFFFFFFF" android:pathData="M20,25 L34,25 L54,66 L74,25 L88,25 L62,82 L46,82 Z"/></vector>`,
+  'mipmap-anydpi-v26/ic_launcher.xml': `<?xml version="1.0" encoding="utf-8"?><adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android"><background android:drawable="@color/vmc_icon_background"/><foreground android:drawable="@drawable/vmc_icon_foreground"/></adaptive-icon>`,
+  'mipmap-anydpi-v26/ic_launcher_round.xml': `<?xml version="1.0" encoding="utf-8"?><adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android"><background android:drawable="@color/vmc_icon_background"/><foreground android:drawable="@drawable/vmc_icon_foreground"/></adaptive-icon>`
+};
+
+for (const [relative, content] of Object.entries(files)) {
+  const target = join(res, relative);
+  await mkdir(join(target, '..'), { recursive: true });
+  await writeFile(target, content, 'utf8');
+}
+console.log('VMC Android branding resources prepared.');

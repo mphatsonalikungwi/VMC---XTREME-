77f0e0209f6221d46b80a99560b1bd921200535c
mphatsonalikungwi <mphatsonalikungwi@gmail.com>
2026-09-16 16:31:38 +0200

    Improve VMC launcher icon safe-zone sizing

--- mobile/scripts/brand-android.mjs
+++ mobile/scripts/brand-android.mjs
@@ -6,7 +6,7 @@ const res = join(root, 'app', 'src', 'main', 'res');
 
 const files = {
   'values/vmc_brand.xml': `<resources><color name="vmc_icon_background">#FF304F</color></resources>`,
-  'drawable/vmc_icon_foreground.xml': `<?xml version="1.0" encoding="utf-8"?><bitmap xmlns:android="http://schemas.android.com/apk/res/android" android:src="@drawable/vmc_logo" android:gravity="center"/>`,
+  'drawable/vmc_icon_foreground.xml': `<?xml version="1.0" encoding="utf-8"?><layer-list xmlns:android="http://schemas.android.com/apk/res/android"><item android:left="18dp" android:top="18dp" android:right="18dp" android:bottom="18dp"><bitmap android:src="@drawable/vmc_logo" android:gravity="center"/></item></layer-list>`,
   'mipmap-anydpi-v26/ic_launcher.xml': `<?xml version="1.0" encoding="utf-8"?><adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android"><background android:drawable="@color/vmc_icon_background"/><foreground android:drawable="@drawable/vmc_icon_foreground"/></adaptive-icon>`,
   'mipmap-anydpi-v26/ic_launcher_round.xml': `<?xml version="1.0" encoding="utf-8"?><adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android"><background android:drawable="@color/vmc_icon_background"/><foreground android:drawable="@drawable/vmc_icon_foreground"/></adaptive-icon>`
 };
@@ -16,4 +16,4 @@ for (const [relative, content] of Object.entries(files)) {
   await mkdir(join(target, '..'), { recursive: true });
   await writeFile(target, content, 'utf8');
 }
-console.log('Official VMC logo launcher resources prepared.');
+console.log('Official VMC logo launcher resources prepared with safe-zone padding.');
